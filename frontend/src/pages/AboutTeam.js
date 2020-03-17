f480932509afb8a2268555d1886fd010aecb4a7d
Pontus Cowling Mantefors <pontuscm@kth.se>
2020-03-17 17:10:04 +0100

    switching to react

---
+++ frontend/src/pages/AboutTeam.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+class AboutTeam extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="mainItem">
+          <small>
+          <br />Pontus Cowling Mantefors
+          <br />Erik Wanhainen
+          <br />Aïssata Maiga
+          <br />Johanna Löv
+          <br />Michael Wink
+          <br />Viktor Åryd
+          <br />Markus Tollet
+          <br />Daniel Onegård
+          </small>
+          <br />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AboutTeam;
