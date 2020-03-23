8b7b6751aa1104f980e5c4bb7fffb0f422309829
dajamante <aimaiga2@gmail.com>
2020-03-23 15:36:58 +0100

    correct path

--- dataprocessing/parser.py
+++ dataprocessing/parser.py
@@ -1,17 +1,17 @@
-class Parser:
+from pathlib import Path
 
+
+class Parser:
     def __init__(self):
-        self.path_text = "/Users/aissata/mySkolfiler2/mvk/MVK-influencers/dataprocessing/classifierad_ord.txt"
+        self.path_text = Path("classifierad_ord.txt")
 
     def parse(self):
         result = []
-        # read binary mode: https://stackoverflow.com/questions/22216076/unicodedecodeerror-utf8-codec-cant-decode-byte-0xa5-in-position-0-invalid-s
         with open(self.path_text, "r") as pt:
             for i in pt.readlines():
                 if len(i.strip()) == 0:
                     continue
                 tmp = i.rstrip().split(" ")
-                tmp_tuple = (tmp[0], tmp[1])
-                # print(tmp_tuple)
-                result.append(tmp_tuple)
-        return result
+                result.append((tmp[0], tmp[1]))
+
+            return result
