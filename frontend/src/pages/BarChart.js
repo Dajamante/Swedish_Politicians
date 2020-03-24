b61debdf3fd242fd95b9f3fa77fac9b094eff389
Pontus Cowling Mantefors <pontuscm@kth.se>
2020-03-24 11:20:40 +0100

    added jsdoc

--- frontend/src/pages/BarChart.js
+++ frontend/src/pages/BarChart.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
 import * as d3 from "d3";
 
+/**
+ * BarChart component drawing a basic example of a D3 bar chart.
+ */
 class BarChart extends Component {
+  /**
+   * Dummy data for D3 bar chart example
+   * @type {Array}
+   */
   componentDidMount() {
     const data = [2, 4, 2, 6, 8, 10, 3, 7, 1, 5, 4, 3, 2];
     this.drawBarChart(data);
   }
+
+  /**
+   * Function that draws the D3 example chart.
+   * @param {Array} data
+   */
   drawBarChart(data) {
     const canvasHeight = 400;
     const canvasWidth = 580;
