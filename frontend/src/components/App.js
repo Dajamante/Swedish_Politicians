0b6ca5039629a374fab7cd2671eb4bcdc1edf225
Pontus Cowling Mantefors <pontuscm@kth.se>
2020-03-25 22:54:20 +0100

    fixing merge conflict

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
-import "../stylesheets/App.scss";
+import "./App.css";
 import NavigationBar from "./NavigationBar";
 import Routes from "./Routes";
 
+/**
+ * Main class App returning the navigation bar component and the routes component.
+ */
 class App extends Component {
   render() {
     return (
