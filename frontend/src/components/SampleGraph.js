ef016ac5babdbcb32611bf486f674d8b04505eb4
Pontus Cowling Mantefors <pontuscm@kth.se>
2020-03-27 16:09:04 +0100

    experimenting with filter for graph

--- frontend/src/components/SampleGraph.js
+++ frontend/src/components/SampleGraph.js
@@ -2,18 +2,54 @@ import React, { Component } from "react";
 import dummy_graph_data from "../data/dummy_graph_data";
 import { ResponsiveLine } from '@nivo/line';
 
-// make sure parent container have a defined height when using
-// responsive component, otherwise height will be 0 and
-// no chart will be rendered.
-// website examples showcase many properties,
-// you'll often use just a few of them.
 class SampleGraph extends Component {
+    constructor() {
+        super();
+        this.state = {
+          filterName: "",
+          filterVehicle: ""
+        };
+    }
+
+    updateFilterName(event) {
+        this.setState({ filterName: event.target.value.substring(0, 50) });
+    }
+
+    //not used yet
+    updateFilterVehicle(event) {
+        this.setState({ filterVehicle: event.target.value.substring(0, 50) });
+    }
+
     render(){
+        let filteredDataByName = dummy_graph_data.filter(listPost => {
+            return (
+              listPost.id
+                .toLowerCase()
+                .indexOf(this.state.filterName.toLowerCase()) !== -1
+            );
+        });
+        
+        if (filteredDataByName == null) {
+            filteredDataByName = dummy_graph_data
+        }
+
         return (
             <div style={{ height: '500px', width: '1000px' }}>
+                Filter by name of country:{" "}
+                <input
+                    type="text"
+                    value={this.state.filterName}
+                    onChange={this.updateFilterName.bind(this)}
+                />
+{/*                 Filter by vehicle:{" "}
+                <input
+                    type="text"
+                    value={this.state.filterVehicle}
+                    onChange={this.updateFilterVehicle.bind(this)}
+                /> */}
                 <ResponsiveLine
-                    data={dummy_graph_data}
-                    margin={{ top: 0, right: 110, bottom: 50, left: 60 }}
+                    data={filteredDataByName}
+                    margin={{ top: 20, right: 110, bottom: 50, left: 60 }}
                     xScale={{ type: 'point' }}
                     yScale={{ type: 'linear', min: 'auto', max: 'auto', stacked: true, reverse: false }}
                     axisTop={null}
