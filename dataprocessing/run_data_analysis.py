2803db976ea8e78fe33bb20c5c5367717ec9db77
Erik Vanhainen <erikwanhainen@gmail.com>
2020-03-27 20:25:53 +0100

    changed file from Markus

--- dataprocessing/run_data_analysis.py
+++ dataprocessing/run_data_analysis.py
@@ -3,23 +3,27 @@
 from model import Model
 from data import Data
 from sklearn.linear_model import SGDClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn import svm
 import os
+import numpy as np
 
 
 def main():
 
     # =====Environmental variables=====
-    host = os.environ.get('DBHOST', 'localhost')
+    host = os.environ['DBHOST']  
     port = '5432'
-    database = os.environ.get('DBDB', 'mvk')
-    user = os.environ.get('DBUSER', 'postgres')
-    password = os.environ.get('DBPASS', '')
+    database = os.environ['DBDB']  
+    user = os.environ['DBUSER']  
+    password = os.environ['DBPASS'] 
     # ================================
 
     # ====Instatiation of Classes====
     model = Model(':/', ':/')
-    database = Data(user=user, host=host, port=port,
-                    database=database, password=password)
+    database = Data(user=str(user), host=str(host), port=str(port),
+                    database=str(database), password=str(password))
+
     # ===============================
 
     # ====Temp test sentances========
@@ -31,11 +35,10 @@ def main():
 
     # Extract data
     df_anforanden = database.get_data_table('anforandetext')
-    anforande_texts = df_anforanden[0]['anforandetext']
+    df_target = database.get_data_table('resultat_sentiment')
+    anforande_texts = df_anforanden['text']
+
     # Build a pipeline
-
-    # TODO: Extract test_sentances from database & enter them into pipeline
-
     text_clf = model.building_pipeline(
         SGDClassifier, test_sentances=test_sentances, test_targets=test_targets)
 
@@ -45,11 +48,24 @@ def main():
     # testing classifier
     docs_test = model.testing
     predicted = text_clf.predict(docs_test)
-    # result
-    res = test.test_classification(predicted)
-    print(res)
 
-    # TODO: Write predicted above to database
+    res_test = model.test_classification(predicted)
+    print(predicted,res_test)
+
+    # strip anförande of <p>,</p>,...
+    anforande_stripped = [anforande.strip('<p>').strip(
+        '</p>').replace('</p><p>', '') for anforande in np.array(anforande_texts)]
+
+    # preict sentiment of anförande
+    predicted_anforande = text_clf.predict(anforande_stripped)
+    # convert to labels to floats
+    predicted_anforande = [float(label) for label in predicted_anforande]
+
+    df_target['anforande_id'] = df_anforanden['anforande_id']
+    df_target['resultat'] = predicted_anforande
+
+    print(df_target)
+    database.insert_data_table(df_target, df_target, 'resultat_sentiment')
 
 
 if __name__ == '__main__':
