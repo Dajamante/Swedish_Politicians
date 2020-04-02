345aa1492aab0b9b14014501684595532cd6b583
Johanna Löv <jolov@kth.se>
2020-04-02 18:23:01 +0200

    Fixed startdate and enddate parameters + two queries for SA with rank + update file for dummy db

--- backend/api/db_dump_noroot.sql
+++ backend/api/db_dump_noroot.sql
@@ -1,124 +1,93 @@
---
--- PostgreSQL database dump
---
+-- *************** SqlDBM: PostgreSQL ****************;
+-- ***************************************************;
 
--- Dumped from database version 11.5
--- Dumped by pg_dump version 12.2
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
+-- ************************************** "riksdagsledamot"
 
-SET default_tablespace = '';
-
---
--- Name: anforandetext; Type: TABLE; Schema: public; Owner: root
---
-
-CREATE TABLE public.anforandetext (
-    anforande_id character varying(150) NOT NULL,
-    text character varying NOT NULL,
-    dokument_id character varying(50) NOT NULL,
-    datum date NOT NULL,
-    person_id bigint NOT NULL
+CREATE TABLE "riksdagsledamot"
+(
+ "person_id" bigint NOT NULL,
+ "namn"      varchar(50) NOT NULL,
+ "parti"     varchar(10) NOT NULL,
+ CONSTRAINT "PK_anforandeperson" PRIMARY KEY ( "person_id" )
 );
 
 
---ALTER TABLE public.anforandetext OWNER TO root;
 
---
--- Name: resultat_sentiment; Type: TABLE; Schema: public; Owner: root
---
+-- ************************************** "voteringar"
 
-CREATE TABLE public.resultat_sentiment (
-    anforande_id character varying(150) NOT NULL,
-    resultat double precision NOT NULL
+CREATE TABLE "voteringar"
+(
+ "voterings_id" varchar(50) NOT NULL,
+ "person_id"    bigint NOT NULL,
+ "vot"          varchar(50) NOT NULL,
+ "vot_datum"    date NOT NULL,
+ CONSTRAINT "PK_voteringar" PRIMARY KEY ( "voterings_id", "person_id" ),
+ CONSTRAINT "FK_73" FOREIGN KEY ( "person_id" ) REFERENCES "riksdagsledamot" ( "person_id" )
+);
+
+CREATE INDEX "fkIdx_73" ON "voteringar"
+(
+ "person_id"
 );
 
 
---ALTER TABLE public.resultat_sentiment OWNER TO root;
 
---
--- Name: riksdagsledamot; Type: TABLE; Schema: public; Owner: root
---
+-- ************************************** "anforandetext"
 
-CREATE TABLE public.riksdagsledamot (
-    person_id bigint NOT NULL,
-    namn character varying(50) NOT NULL,
-    parti character varying(10) NOT NULL
+CREATE TABLE "anforandetext"
+(
+ "anforande_id" varchar(150) NOT NULL,
+ "text"         varchar NOT NULL,
+ "datum"        date NOT NULL,
+ "person_id"    bigint NOT NULL,
+ CONSTRAINT "PK_anforande" PRIMARY KEY ( "anforande_id" ),
+ CONSTRAINT "FK_20" FOREIGN KEY ( "person_id" ) REFERENCES "riksdagsledamot" ( "person_id" )
+);
+
+CREATE INDEX "fkIdx_20" ON "anforandetext"
+(
+ "person_id"
 );
 
 
---ALTER TABLE public.riksdagsledamot OWNER TO root;
 
---
--- Name: anforandetext pk_anforande; Type: CONSTRAINT; Schema: public; Owner: root
---
+-- ************************************** "resultat_votering"
 
-ALTER TABLE ONLY public.anforandetext
-    ADD CONSTRAINT pk_anforande PRIMARY KEY (anforande_id);
+CREATE TABLE "resultat_votering"
+(
+ "voterings_id" varchar(50) NOT NULL,
+ "person_id"    bigint NOT NULL,
+ "vot"          varchar(50) NOT NULL,
+ CONSTRAINT "PK_resultat_votering" PRIMARY KEY ( "voterings_id", "person_id" ),
+ CONSTRAINT "FK_84" FOREIGN KEY ( "voterings_id", "person_id" ) REFERENCES "voteringar" ( "voterings_id", "person_id" )
+);
 
-
---
--- Name: riksdagsledamot pk_anforandeperson; Type: CONSTRAINT; Schema: public; Owner: root
---
-
-ALTER TABLE ONLY public.riksdagsledamot
-    ADD CONSTRAINT pk_anforandeperson PRIMARY KEY (person_id);
-
-
---
--- Name: resultat_sentiment pk_resultat; Type: CONSTRAINT; Schema: public; Owner: root
---
-
-ALTER TABLE ONLY public.resultat_sentiment
-    ADD CONSTRAINT pk_resultat PRIMARY KEY (anforande_id);
+CREATE INDEX "fkIdx_84" ON "resultat_votering"
+(
+ "voterings_id",
+ "person_id"
+);
 
 
---
--- Name: fkidx_20; Type: INDEX; Schema: public; Owner: root
---
+-- ************************************** "resultat_sentiment"
 
-CREATE INDEX fkidx_20 ON public.anforandetext USING btree (person_id);
+CREATE TABLE "resultat_sentiment"
+(
+ "anforande_id" varchar(150) NOT NULL,
+ "resultat"     float NOT NULL,
+ CONSTRAINT "PK_resultat" PRIMARY KEY ( "anforande_id" ),
+ CONSTRAINT "FK_31" FOREIGN KEY ( "anforande_id" ) REFERENCES "anforandetext" ( "anforande_id" )
+);
 
-
---
--- Name: fkidx_31; Type: INDEX; Schema: public; Owner: root
---
-
-CREATE INDEX fkidx_31 ON public.resultat_sentiment USING btree (anforande_id);
-
-
---
--- Name: anforandetext fk_20; Type: FK CONSTRAINT; Schema: public; Owner: root
---
-
-ALTER TABLE ONLY public.anforandetext
-    ADD CONSTRAINT fk_20 FOREIGN KEY (person_id) REFERENCES public.riksdagsledamot(person_id);
-
-
---
--- Name: resultat_sentiment fk_31; Type: FK CONSTRAINT; Schema: public; Owner: root
---
-
-ALTER TABLE ONLY public.resultat_sentiment
-    ADD CONSTRAINT fk_31 FOREIGN KEY (anforande_id) REFERENCES public.anforandetext(anforande_id);
-
-
---
--- Name: SCHEMA public; Type: ACL; Schema: -; Owner: root
---
+CREATE INDEX "fkIdx_31" ON "resultat_sentiment"
+(
+ "anforande_id"
+);
 
 
 
---
--- PostgreSQL database dump complete
---
+
+
+
 
