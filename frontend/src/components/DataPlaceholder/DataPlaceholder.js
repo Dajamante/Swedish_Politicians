4fc3959dba613b446418eec50e14026f734dff39
miwink <michael.w.99@hotmail.com>
2020-04-27 23:54:02 +0200

    Added DataPlaceHolder component

---
+++ frontend/src/components/DataPlaceholder/DataPlaceholder.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./DataPlaceholder.scss";
+
+const DataPlaceholder = () => {
+  return (
+    <>
+      <div className="homeMainWindowTopLine"></div>
+      <div className="homeMainWindow"></div>
+    </>
+  );
+};
+
+export default DataPlaceholder;
