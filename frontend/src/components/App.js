c01e2bbbd2b67896534b18027252cfaff32c2e30
miwink <michael.w.99@hotmail.com>
2020-04-28 22:12:19 +0200

    Created a footer

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,6 +3,7 @@ import { withRouter } from "react-router-dom";
 import "../stylesheets/stylesheet.scss";
 import NavBar from "./NavBar/NavBar";
 import Routes from "./Routes";
+import Footer from "./Footer/Footer";
 
 /**
  * Main class App returning the navigation bar component and the routes component.
@@ -14,6 +15,7 @@ class App extends Component {
       <div className="App">
         <NavBar />
         <RoutesSync />
+        <Footer />
       </div>
     );
   }
