c16a2a61097de0b8ef900e436611a2865d01976f
Pontus Cowling Mantefors <pontuscm@kth.se>
2020-05-11 16:19:23 +0200

    implementerat om-sidan saknas foton

--- frontend/src/components/Routes.js
+++ frontend/src/components/Routes.js
@@ -2,19 +2,13 @@ import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import TopLists from "../pages/TopLists/TopLists";
-import AboutTeam from "../pages/AboutTeam";
-import AboutProject from "../pages/AboutProject";
-import AboutFindwise from "../pages/AboutFindwise";
-import Sample from "../pages/Sample";
+import About from "../pages/About/About";
 
 const Routes = () => (
   <Switch>
     <Route path="/home" component={Home} />
     <Route path="/toplists" component={TopLists} />
-    <Route path="/aboutTeam" component={AboutTeam} />
-    <Route path="/aboutProject" component={AboutProject} />
-    <Route path="/aboutFindwise" component={AboutFindwise} />
-    <Route path="/sample" component={Sample} />
+    <Route path="/about" component={About} />
     <Route component={Home} />
     <Redirect to="/home" />
   </Switch>
